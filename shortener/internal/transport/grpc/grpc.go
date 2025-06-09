024481a45208797d18315a0fd6754edc4220deb1
misshanya <mishanyadev@gmail.com>
2025-06-09 19:05:02 +0300

    feat(shortener): validate url

--- shortener/internal/transport/grpc/grpc.go
+++ shortener/internal/transport/grpc/grpc.go
@@ -5,6 +5,9 @@ import (
 	pb "github.com/misshanya/url-shortener/gen/go/v1"
 	"github.com/misshanya/url-shortener/shortener/internal/models"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"net/url"
 )
 
 type service interface {
@@ -25,6 +28,11 @@ func NewHandler(grpcServer *grpc.Server, service service) {
 func (h *Handler) ShortURL(ctx context.Context, req *pb.ShortURLRequest) (*pb.ShortURLResponse, error) {
 	short := models.Short{URL: req.Url}
 
+	// Validate URL
+	if _, err := url.ParseRequestURI(short.URL); err != nil {
+		return nil, status.Error(codes.InvalidArgument, "bad URL")
+	}
+
 	if err := h.service.ShortURL(ctx, &short); err != nil {
 		return nil, err
 	}
@@ -35,10 +43,10 @@ func (h *Handler) ShortURL(ctx context.Context, req *pb.ShortURLRequest) (*pb.Sh
 func (h *Handler) GetURL(ctx context.Context, req *pb.GetURLRequest) (*pb.GetURLResponse, error) {
 	hash := req.Hash
 
-	url, err := h.service.GetURL(ctx, hash)
+	originalURL, err := h.service.GetURL(ctx, hash)
 	if err != nil {
 		return nil, err
 	}
 
-	return &pb.GetURLResponse{Url: url}, nil
+	return &pb.GetURLResponse{Url: originalURL}, nil
 }
