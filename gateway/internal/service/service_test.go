296e4da23af473e8ad50dd41f613c187020b5e38
misshanya <mishanyadev@gmail.com>
2025-07-23 16:11:03 +0300

    test(gateway): add tests for ShortenURLBatch method in service layer

--- gateway/internal/service/service_test.go
+++ gateway/internal/service/service_test.go
@@ -136,6 +136,126 @@ func Test_ShortenURL(t *testing.T) {
 	}
 }
 
+func Test_ShortenURLBatch(t *testing.T) {
+	type inputModel struct {
+		OriginalURL string
+		Code        string
+		Error       string
+	}
+
+	tests := []struct {
+		Name        string
+		PublicHost  string
+		InputURLs   []inputModel
+		ExceptedErr *models.HTTPError
+		SetUpMocks  func(client *mockgrpcClient, inputs []inputModel)
+	}{
+		{
+			Name:       "Successfully Shortened 2 of 3",
+			PublicHost: "https://sh.some/",
+			InputURLs: []inputModel{
+				{
+					OriginalURL: "https://go.dev",
+					Code:        "3a",
+				},
+				{
+					OriginalURL: "https://github.com",
+					Error:       "some error",
+				},
+				{
+					OriginalURL: "https://gitlab.com",
+					Code:        "3c",
+				},
+			},
+			ExceptedErr: nil,
+			SetUpMocks: func(client *mockgrpcClient, inputs []inputModel) {
+				urlsForReq := make([]*pb.ShortenURLRequest, len(inputs))
+				for i, input := range inputs {
+					urlsForReq[i] = &pb.ShortenURLRequest{Url: input.OriginalURL}
+				}
+
+				urlsForResp := make([]*pb.ShortenURLResponse, len(inputs))
+				for i, input := range inputs {
+					urlsForResp[i] = &pb.ShortenURLResponse{
+						Code:        input.Code,
+						OriginalUrl: input.OriginalURL,
+						Error:       input.Error,
+					}
+				}
+
+				client.On("ShortenURLBatch", mock.Anything, &pb.ShortenURLBatchRequest{Urls: urlsForReq}).
+					Return(
+						&pb.ShortenURLBatchResponse{
+							Urls: urlsForResp,
+						},
+						nil,
+					).Once()
+			},
+		},
+		{
+			Name:      "gRPC server returned slice not the same length",
+			InputURLs: make([]inputModel, 5),
+			ExceptedErr: &models.HTTPError{
+				Code:    http.StatusInternalServerError,
+				Message: "Internal Server Error",
+			},
+			SetUpMocks: func(client *mockgrpcClient, inputs []inputModel) {
+				client.On("ShortenURLBatch", mock.Anything, mock.Anything).
+					Return(
+						&pb.ShortenURLBatchResponse{
+							Urls: make([]*pb.ShortenURLResponse, len(inputs)-1),
+						},
+						nil,
+					).Once()
+			},
+		},
+		{
+			Name:      "gRPC server answered with error",
+			InputURLs: make([]inputModel, 2),
+			ExceptedErr: &models.HTTPError{
+				Code:    http.StatusInternalServerError,
+				Message: "Something went wrong",
+			},
+			SetUpMocks: func(client *mockgrpcClient, inputs []inputModel) {
+				client.On("ShortenURLBatch", mock.Anything, mock.Anything).
+					Return(
+						nil,
+						status.New(codes.Internal, "Something went wrong").Err(),
+					).Once()
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			mockClient := mockgrpcClient{}
+
+			tt.SetUpMocks(&mockClient, tt.InputURLs)
+
+			service := NewService(&mockClient, tt.PublicHost)
+
+			shorts := make([]*models.Short, len(tt.InputURLs))
+			for i, input := range tt.InputURLs {
+				shorts[i] = &models.Short{
+					OriginalURL: input.OriginalURL,
+				}
+			}
+			err := service.ShortenURLBatch(context.Background(), shorts)
+			assert.Equal(t, tt.ExceptedErr, err)
+
+			for i := range shorts {
+				assert.Equal(t, tt.InputURLs[i].OriginalURL, shorts[i].OriginalURL)
+				assert.Equal(t, tt.InputURLs[i].Error, shorts[i].Error)
+				if tt.InputURLs[i].Error == "" {
+					assert.Equal(t, tt.PublicHost+tt.InputURLs[i].Code, shorts[i].ShortURL)
+				}
+			}
+
+			mockClient.AssertExpectations(t)
+		})
+	}
+}
+
 func Test_UnshortenURL(t *testing.T) {
 	tests := []struct {
 		Name           string
